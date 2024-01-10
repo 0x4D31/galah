f55b5b5f3b2cd950e432fdd6b08dcfb4d2240ed8
Adel K <adel.net@gmail.com>
2024-01-10 20:47:06 +0100

    Remove the unused structs.

--- openai.go
+++ openai.go
@@ -12,21 +12,6 @@ import (
 	openai "github.com/sashabaranov/go-openai"
 )
 
-type OpenAIResponse struct {
-	ID      string                 `json:"id"`
-	Object  string                 `json:"object"`
-	Created int                    `json:"created"`
-	Model   string                 `json:"model"`
-	Choices []OpenAIResponseChoice `json:"choices"`
-}
-
-type OpenAIResponseChoice struct {
-	Text         string      `json:"text"`
-	Index        int         `json:"index"`
-	Logprobs     interface{} `json:"logprobs"`
-	FinishReason string      `json:"finish_reason"`
-}
-
 func GenerateOpenAIResponse(cfg *Config, r *http.Request) (string, error) {
 	// Create a prompt based on the HTTP request
 	httpReq, err := httputil.DumpRequest(r, true)
