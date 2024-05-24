dcbdf89fde95dca0fa9b1311c05ee1514e3b2f28
Adel K <adel.net@gmail.com>
2024-05-24 04:48:03 +0200

    Add Server package with tests and improve readability.

---
+++ internal/server/server.go
@@ -0,0 +1,218 @@
+package server
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/0x4d31/galah/internal/cache"
+	"github.com/0x4d31/galah/internal/config"
+	"github.com/0x4d31/galah/internal/logger"
+	"github.com/0x4d31/galah/pkg/llm"
+	"github.com/sirupsen/logrus"
+	"github.com/tmc/langchaingo/llms"
+	"golang.org/x/sync/errgroup"
+)
+
+var ignoreHeaders = map[string]bool{
+	// Standard headers to ignore
+	"content-length": true,
+	"content-type":   true,
+	"date":           true,
+	"expires":        true,
+	"last-modified":  true,
+	// OpenAI made-up headers to ignore
+	"http":     true,
+	"http/1.0": true,
+	"http/1.1": true,
+	"http/1.2": true,
+	"http/2.0": true,
+}
+
+type Server struct {
+	Cache       *sql.DB
+	Config      config.Config
+	EventLogger *logger.Logger
+	LLMConfig   llm.Config
+	Logger      *logrus.Logger
+	Model       llms.Model
+	Servers     map[uint16]*http.Server
+}
+
+func (s *Server) StartServers() error {
+	var g errgroup.Group
+	mu := sync.Mutex{}
+
+	for _, pc := range s.Config.Ports {
+		pc := pc // Capture the loop variable
+		g.Go(func() error {
+			return s.startServer(pc, &mu)
+		})
+	}
+
+	return g.Wait()
+}
+
+func (s *Server) startServer(pc config.PortConfig, mu *sync.Mutex) error {
+	server := s.SetupServer(pc)
+
+	var err error
+	switch pc.Protocol {
+	case "TLS":
+		err = s.StartTLSServer(server, pc)
+	case "HTTP":
+		err = s.StartHTTPServer(server, pc)
+	default:
+		err = fmt.Errorf("unknown protocol for port %d", pc.Port)
+	}
+	if err != nil {
+		s.Logger.Errorf("error starting server on port %d: %s", pc.Port, err)
+		return err
+	}
+
+	mu.Lock()
+	s.Servers[pc.Port] = server
+	mu.Unlock()
+
+	return nil
+}
+
+func (s *Server) SetupServer(pc config.PortConfig) *http.Server {
+	serverAddr := fmt.Sprintf(":%d", pc.Port)
+	return &http.Server{
+		Addr: serverAddr,
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			s.handleRequest(w, r, serverAddr)
+		}),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+}
+
+func (s *Server) StartTLSServer(server *http.Server, pc config.PortConfig) error {
+	if pc.TLSProfile == "" {
+		return fmt.Errorf("TLS profile is not configured for port %d", pc.Port)
+	}
+
+	tlsConfig, ok := s.Config.TLS[pc.TLSProfile]
+	if !ok || tlsConfig.Certificate == "" || tlsConfig.Key == "" {
+		return fmt.Errorf("TLS profile is incomplete for port %d", pc.Port)
+	}
+
+	s.Logger.Infof("starting HTTPS server on port %d with TLS profile: %s", pc.Port, pc.TLSProfile)
+	return server.ListenAndServeTLS(tlsConfig.Certificate, tlsConfig.Key)
+}
+
+func (s *Server) StartHTTPServer(server *http.Server, pc config.PortConfig) error {
+	s.Logger.Infof("starting HTTP server on port %d", pc.Port)
+	return server.ListenAndServe()
+}
+
+func (s *Server) handleRequest(w http.ResponseWriter, r *http.Request, serverAddr string) {
+	port := s.extractPort(serverAddr)
+	s.Logger.Infof("port %s received a request for %q, from source %s", port, r.URL.String(), r.RemoteAddr)
+
+	response, err := cache.CheckCache(s.Cache, r, port, s.Config.CacheDuration)
+	if err != nil {
+		s.Logger.Infof("request cache miss for %q: %s", r.URL.String(), err)
+	}
+
+	if response == nil {
+		response, err = s.generateResponse(r, port)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	var respData llm.JSONResponse
+	if err := json.Unmarshal(response, &respData); err != nil {
+		s.Logger.Errorf("error unmarshalling the json-encoded data: %s", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	s.sendResponse(w, respData)
+	s.Logger.Infof("sent the generated response to %s", r.RemoteAddr)
+	s.EventLogger.LogEvent(r, respData, port)
+}
+
+func (s *Server) extractPort(serverAddr string) string {
+	_, port, err := net.SplitHostPort(serverAddr)
+	if err != nil {
+		port = ""
+	}
+	return port
+}
+
+func (s *Server) generateResponse(r *http.Request, port string) ([]byte, error) {
+	messages, err := llm.CreateMessageContent(r, s.Config.PromptTemplate, s.LLMConfig.Provider)
+	if err != nil {
+		s.Logger.Errorf("error creating llm message: %s", err)
+		return nil, err
+	}
+
+	responseString, err := llm.GenerateLLMResponse(r.Context(), s.Model, s.LLMConfig.Temperature, messages)
+	if err != nil {
+		s.Logger.Errorf("error generating response: %s", err)
+		s.EventLogger.LogError(r, responseString, port, err)
+		return nil, err
+	}
+
+	s.Logger.Infof("generated HTTP response: %s", strings.ReplaceAll(responseString, "\n", " "))
+
+	response := []byte(responseString)
+	key := cache.GetCacheKey(r, port)
+	if err := cache.StoreResponse(s.Cache, key, response); err != nil {
+		s.Logger.Errorf("error storing response in cache: %s", err)
+	}
+
+	return response, nil
+}
+
+func (s *Server) sendResponse(w http.ResponseWriter, response llm.JSONResponse) {
+	for key, value := range response.Headers {
+		if !isExcludedHeader(key) {
+			w.Header().Set(key, value)
+		}
+	}
+
+	if _, err := w.Write([]byte(response.Body)); err != nil {
+		s.Logger.Errorf("error writing response: %s", err)
+	}
+}
+
+func isExcludedHeader(headerKey string) bool {
+	return ignoreHeaders[strings.ToLower(headerKey)]
+}
+
+func (s *Server) ListenForShutdownSignals() {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-sig
+		s.Logger.Infof("received shutdown signal. shutting down servers...")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		for _, server := range s.Servers {
+			if err := server.Shutdown(ctx); err != nil {
+				s.Logger.Errorf("error shutting down server: %s", err)
+			}
+		}
+
+		s.Logger.Infoln("all servers shut down gracefully.")
+		os.Exit(0)
+	}()
+}
---
+++ internal/server/server_test.go
@@ -0,0 +1,166 @@
+package server_test
+
+import (
+	"context"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/0x4d31/galah/internal/app"
+	"github.com/0x4d31/galah/internal/config"
+	"github.com/0x4d31/galah/internal/server"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sync/errgroup"
+)
+
+func TestStartServers(t *testing.T) {
+	tests := []struct {
+		name       string
+		portConfig config.PortConfig
+		wantErr    bool
+	}{
+		{
+			name: "startServerUnknownProtocol",
+			portConfig: config.PortConfig{
+				Port:     8081,
+				Protocol: "UNKNOWN_PROTOCOL",
+			},
+			wantErr: true,
+		},
+	}
+
+	a := &app.App{
+		Config: config.Config{},
+		Logger: logrus.New(),
+	}
+
+	for _, tt := range tests {
+		a.Config.Ports = []config.PortConfig{tt.portConfig}
+		t.Run(tt.name, func(t *testing.T) {
+
+			srv := server.Server{
+				Cache:       a.Cache,
+				Config:      a.Config,
+				EventLogger: a.EventLogger,
+				LLMConfig:   a.LLMConfig,
+				Logger:      a.Logger,
+				Model:       a.Model,
+			}
+			err := srv.StartServers()
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("StartServers() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestStartHTTPServer(t *testing.T) {
+	tests := []struct {
+		name       string
+		portConfig config.PortConfig
+		wantErr    bool
+	}{
+		{
+			name: "startServerOnOccupiedPort",
+			portConfig: config.PortConfig{
+				Port:     8080, // Use a known port
+				Protocol: "HTTP",
+			},
+			wantErr: true,
+		},
+	}
+
+	a := &app.App{
+		Config: config.Config{},
+		Logger: logrus.New(),
+	}
+
+	// Start a dummy server on port 8080 to occupy it
+	dummyServer := &http.Server{Addr: ":8080"}
+	go dummyServer.ListenAndServe()
+	time.Sleep(100 * time.Millisecond)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			srv := server.Server{
+				Cache:       a.Cache,
+				Config:      a.Config,
+				EventLogger: a.EventLogger,
+				LLMConfig:   a.LLMConfig,
+				Logger:      a.Logger,
+				Model:       a.Model,
+			}
+			HTTPServer := srv.SetupServer(tt.portConfig)
+
+			var g errgroup.Group
+			g.Go(func() error {
+				return srv.StartHTTPServer(HTTPServer, tt.portConfig)
+			})
+
+			err := g.Wait()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("startHTTPServer() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+
+	dummyServer.Shutdown(context.Background())
+}
+
+func TestStartTLSServer(t *testing.T) {
+	tests := []struct {
+		name       string
+		portConfig config.PortConfig
+		wantErr    bool
+	}{
+		{
+			name: "startServerWithInvalidTLSProfile",
+			portConfig: config.PortConfig{
+				Port:       8444,
+				Protocol:   "TLS",
+				TLSProfile: "invalidTLSProfile",
+			},
+			wantErr: true,
+		},
+		{
+			name: "startServerWithMissingTLSProfile",
+			portConfig: config.PortConfig{
+				Port:     8445,
+				Protocol: "TLS",
+			},
+			wantErr: true,
+		},
+	}
+
+	a := &app.App{
+		Config: config.Config{
+			TLS: map[string]config.TLSConfig{
+				"invalidTLSProfile": {
+					Certificate: "non-existent_cert.pem",
+					Key:         "non-existent_key.pem",
+				},
+			},
+		},
+		Logger: logrus.New(),
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			srv := server.Server{
+				Cache:       a.Cache,
+				Config:      a.Config,
+				EventLogger: a.EventLogger,
+				LLMConfig:   a.LLMConfig,
+				Logger:      a.Logger,
+				Model:       a.Model,
+			}
+			HTTPServer := srv.SetupServer(tt.portConfig)
+			err := srv.StartTLSServer(HTTPServer, tt.portConfig)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("startTLSServer() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
