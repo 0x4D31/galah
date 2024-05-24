c9ae83d695885ff5a7b40b10d0ed8e4d7a08a31b
Adel K <adel.net@gmail.com>
2024-05-24 04:50:24 +0200

    Move cache.go into cache package.

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,4 +1,4 @@
-package main
+package cache
 
 import (
 	"database/sql"
@@ -6,11 +6,13 @@ import (
 	"net/http"
 	"sync"
 	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var mutex sync.Mutex
 
-func initializeCache(path string) (*sql.DB, error) {
+func InitializeCache(path string) (*sql.DB, error) {
 	db, err := sql.Open("sqlite3", path)
 	if err != nil {
 		return nil, err
@@ -31,39 +33,39 @@ func initializeCache(path string) (*sql.DB, error) {
 	return db, nil
 }
 
-func (app *App) checkCache(r *http.Request, port string) ([]byte, error) {
+func CheckCache(client *sql.DB, r *http.Request, port string, cacheDuration int) ([]byte, error) {
 	mutex.Lock()
 	defer mutex.Unlock()
 
-	cacheKey := getCacheKey(r, port)
+	cacheKey := GetCacheKey(r, port)
 	var response []byte
 	var cachedAt time.Time
 
 	// Order by cachedAt DESC to get the most recent record.
-	row := app.Cache.QueryRow("SELECT cachedAt, response FROM cache WHERE key = ? ORDER BY cachedAt DESC LIMIT 1", cacheKey)
+	row := client.QueryRow("SELECT cachedAt, response FROM cache WHERE key = ? ORDER BY cachedAt DESC LIMIT 1", cacheKey)
 
 	err := row.Scan(&cachedAt, &response)
 	if err == sql.ErrNoRows {
 		return nil, errors.New("not found in cache")
 	}
 	// TODO: Add an option to disable caching or set an indefinite caching (no expiration).
-	if time.Since(cachedAt) > time.Duration(app.Config.CacheDuration)*time.Hour {
+	if time.Since(cachedAt) > time.Duration(cacheDuration)*time.Hour {
 		return nil, errors.New("cached record is too old")
 	}
 
 	return response, err
 }
 
-func getCacheKey(r *http.Request, port string) string {
+func GetCacheKey(r *http.Request, port string) string {
 	return port + "_" + r.URL.String()
 }
 
-func (app *App) storeResponse(key string, resp []byte) error {
+func StoreResponse(client *sql.DB, key string, resp []byte) error {
 	mutex.Lock()
 	defer mutex.Unlock()
 
 	currentTime := time.Now()
-	_, err := app.Cache.Exec("INSERT OR REPLACE INTO cache (cachedAt, key, response) VALUES (?, ?, ?)", currentTime, key, resp)
+	_, err := client.Exec("INSERT OR REPLACE INTO cache (cachedAt, key, response) VALUES (?, ?, ?)", currentTime, key, resp)
 	if err != nil {
 		return err
 	}
